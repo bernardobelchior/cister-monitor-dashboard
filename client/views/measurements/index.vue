0db182448b71f0922bd64fbdc63f9ba87c09bcdc
Bernardo Belchior <bernardo.belchior1@gmail.com>
2017-11-27 21:23:48 +0000

    Added date formatting in measurements

--- client/views/measurements/index.vue
+++ client/views/measurements/index.vue
@@ -6,31 +6,31 @@
           <h4 class="title">Table</h4>
           <table class="table">
             <thead>
-              <tr>
-                <th>ID</th>
-                <th>Mote ID</th>
-                <th>Timestamp</th>
-                <th>Temperature</th>
-                <th>Humidity</th>
-              </tr>
+            <tr>
+              <th>ID</th>
+              <th>Mote ID</th>
+              <th>Timestamp</th>
+              <th>Temperature</th>
+              <th>Humidity</th>
+            </tr>
             </thead>
             <tfoot>
-              <tr>
-                <th>ID</th>
-                <th>Mote ID</th>
-                <th>Timestamp</th>
-                <th>Temperature</th>
-                <th>Humidity</th>
-              </tr>
+            <tr>
+              <th>ID</th>
+              <th>Mote ID</th>
+              <th>Timestamp</th>
+              <th>Temperature</th>
+              <th>Humidity</th>
+            </tr>
             </tfoot>
             <tbody>
-              <tr v-for="measurement in measurements" v-bind:key="measurement.id">
-                <td>{{measurement.id}}</td>
-                <td>{{measurement.mote_id}}</td>
-                <td>{{measurement.date}}</td>
-                <td>{{measurement.temperature}}ºC</td>
-                <td>{{measurement.humidity}} %</td>
-              </tr>
+            <tr v-for="measurement in measurements" v-bind:key="measurement.id">
+              <td>{{measurement.id}}</td>
+              <td>{{measurement.mote_id}}</td>
+              <td>{{measurement.date}}</td>
+              <td>{{measurement.temperature}}ºC</td>
+              <td>{{measurement.humidity}} %</td>
+            </tr>
             </tbody>
           </table>
         </article>
@@ -40,34 +40,33 @@
 </template>
 
 <script>
-var request = require('request')
-export default {
-  data() {
-    return {
-      measurements: []
-    }
-  },
-  mounted() {
-    let self = this
-    request({
-      url: process.env.HOST_URL + '/measurements/10',
-      method: 'GET'
-    }, function(error, response, body) {
-      self.measurements = JSON.parse(body).results
-      for (let measurement in self.measurements) {
-        measurement.date = new Date(measurement.time_stamp).toLocaleTimeString()
+  import axios from 'axios'
+
+  export default {
+    data () {
+      return {
+        measurements: []
       }
-    })
+    },
+    mounted () {
+      let self = this
+      axios.get(process.env.HOST_URL + '/measurements', {params: {numResults: 10}})
+        .then((response) => {
+          self.measurements = response.data
+          for (let measurement of self.measurements) {
+            measurement.date = new Date(measurement.time_stamp).toLocaleString()
+          }
+        }).catch((error) => console.log(error))
+    }
   }
-}
 </script>
 
 
 <style lang="scss">
-.table-responsive {
-  display: block;
-  width: 100%;
-  min-height: .01%;
-  overflow-x: auto;
-}
+  .table-responsive {
+    display: block;
+    width: 100%;
+    min-height: .01%;
+    overflow-x: auto;
+  }
 </style>
